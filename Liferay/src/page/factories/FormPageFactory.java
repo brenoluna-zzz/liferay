52ea792b76d98a9b8de14e8043ef979f188897c5
Breno Luna <brenoefl@gmail.com>
2018-05-05 14:52:49 -0300

    Created Form's Page Factory class

---
+++ Liferay/src/page/factories/FormPageFactory.java
@@ -0,0 +1,46 @@
+package page.factories;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormPageFactory {
+
+	WebDriver driver;
+	
+	public FormPageFactory(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this); // Initializes the WebElements in this class
+	}
+	
+	// Page Elements
+	@FindBy(id="_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_ddm$$Qual…SeuNome$RTcoaxkf$0$$en_US")
+	WebElement nameField;
+	
+	@FindBy(xpath="//div[@id='yui_patched_v3_18_1_1_1525541902199_1271']//input")
+	WebElement dobField;
+	
+	@FindBy(id="_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_ddm$$PorqueVocÍIngressouNa¡reaDeTestes$htHBdPf1$0$$en_US")
+	WebElement textArea;
+	
+	@FindBy(xpath="//button[contains(text(),'Submit')]")
+	WebElement submitBtn;
+	
+	// Page Elements' methods
+	public void setNameField(String name) {
+		nameField.sendKeys(name);
+	}
+	
+	public void setDobField(String dob) {
+		dobField.sendKeys(dob);
+	}
+	
+	public void setTextArea(String text) {
+		textArea.sendKeys(text);
+	}
+	
+	public void clickSubmitBtn() {
+		submitBtn.click();
+	}
+}
